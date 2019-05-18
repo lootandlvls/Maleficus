70f6a46a02823077218fc721999fcf6d5c50cabb
BNJMO <dj_bnjmo@hotmail.fr>
2019-05-18 15:29:40 +0200

    Relocated HitInfo under MaleficusTypes

--- Maleficus/Maleficus/Assets/Scripts/Managers/MaleficusTypes.cs
+++ Maleficus/Maleficus/Assets/Scripts/Managers/MaleficusTypes.cs
@@ -23,6 +23,29 @@ public enum PlayerID
     PLAYER_4,
     TEST
 }
+
+public struct HitInfo
+{
+    public HitInfo(ISpell castedSpell, PlayerID castingPlayerID, PlayerID hitplayerID, Vector3 hitPosition)
+    {
+        this.castedSpell = castedSpell;
+        this.castingPlayerID = castingPlayerID;
+        this.hitPlayerID = hitplayerID;
+        this.hitPosition = hitPosition;
+    }
+
+    public ISpell CastedSpell { get { return castedSpell; } }
+    public PlayerID CastingPlayerID { get { return castingPlayerID; } }
+    public PlayerID HitPlayerID { get { return hitPlayerID; } }
+    public Vector3 HitPosition { get { return hitPosition; } }
+    public Vector3 HitVelocity { get { return hitPosition + castedSpell.Direction * castedSpell.HitPower; } }
+
+
+    private ISpell castedSpell;
+    private PlayerID castingPlayerID;
+    private PlayerID hitPlayerID;
+    private Vector3 hitPosition;
+}
 #endregion 
 
 #region Input
