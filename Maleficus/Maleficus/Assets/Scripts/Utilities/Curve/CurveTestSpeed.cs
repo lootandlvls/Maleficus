26580c5292f800495635b693549157c9499c5489
BNJMO <dj_bnjmo@hotmail.fr>
2019-06-14 20:59:44 +0200

    Implement curve reader

---
+++ Maleficus/Maleficus/Assets/Scripts/Utilities/Curve/CurveTestSpeed.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveTestSpeed : MonoBehaviour
+{
+    [SerializeField] private Transform startTransform;
+    [SerializeField] private Transform endTransform;
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private float heightFactor = 1.0f;
+
+    private bool isStarted = false;
+
+    private float movementDuration;
+    private float startTime;
+
+    private float timeProgress;
+    private float percentageProgress;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            StartMovement();
+        }
+
+        if (isStarted == true)
+        {
+            timeProgress = Time.time - startTime;
+            percentageProgress = timeProgress / movementDuration;
+
+            if (percentageProgress >= 1.0f)
+            {
+                isStarted = false;
+                return;
+            }
+
+            float curveValue = CurveReader.Instance.EvaluateCurve(3, percentageProgress);
+            float heightValue = CurveReader.Instance.EvaluateCurve(1, percentageProgress);
+
+    
+            transform.position = Vector3.Lerp(startTransform.position, endTransform.position, curveValue);
+            transform.rotation = Quaternion.Lerp(startTransform.rotation, endTransform.rotation, curveValue);
+
+            transform.position = new Vector3(transform.position.x, heightValue * heightFactor, transform.position.z);
+        }
+
+
+    }
+
+    private void StartMovement()
+    {
+        isStarted = true;
+        startTime = Time.time;
+
+        float trajectoryDistance = Vector3.Distance(startTransform.position, endTransform.position);
+        movementDuration = trajectoryDistance / movementSpeed;
+
+        transform.position = startTransform.position;
+        transform.rotation = endTransform.rotation;
+    }
+}
