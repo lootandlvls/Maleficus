1c2040259bf59be98a929910a73d1b1cad6ccc23
leon <ga84teh@mytum.de>
2019-06-17 10:44:27 +0200

    added my netcode to the party; open lobby scene to start;

---
+++ Maleficus/Maleficus/Assets/Scripts/Networking/Shared/Net_Message/Net_OnCreateAccount.cs
@@ -0,0 +1,14 @@
+﻿[System.Serializable]
+public class Net_OnCreateAccount : NetMsg
+{
+    public Net_OnCreateAccount()
+    {
+        OP = NetOP.OnCreateAccount;
+    }
+    public byte Success { set; get; }
+    public string Information { set; get; }
+    
+    public int ConnectionId { set; get; }
+    public string Username { set; get; }
+    public string Discriminator { set; get; }
+}
---
+++ Maleficus/Maleficus/Assets/Scripts/UI/UIScenes/HubScene.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HubScene : MonoBehaviour
+{
+    public static HubScene Instance { set; get; }
+
+    [SerializeField] private TextMeshProUGUI selfInformation;
+    [SerializeField] private TMP_InputField addFollowInput;
+
+    [SerializeField] private GameObject followPrefab;
+    [SerializeField] private Transform followContainer;
+
+    private Dictionary<string, GameObject> uiFollows = new Dictionary<string, GameObject>();
+
+    private void Start()
+    {
+        Instance = this;
+        selfInformation.text = Client.Instance.self.Username + "#" + Client.Instance.self.Discriminator;
+        Client.Instance.SendRequestFollow();
+    }
+
+    public void AddFollowToUi(Account follow)
+    {
+        GameObject followItem = Instantiate(followPrefab, followContainer);
+
+        followItem.GetComponentInChildren<TextMeshProUGUI>().text = follow.Username + "#" + follow.Discriminator;
+        followItem.transform.GetChild(1).GetComponent<Image>().color = (follow.Status != 0) ? Color.green : Color.gray;
+        followItem.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { Destroy(followItem); });
+        //Todo wait for server response bevor deleting object
+        followItem.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(delegate { OnClickRemoveFollow(follow.Username, follow.Discriminator); });
+
+        uiFollows.Add(follow.Username + "#" + follow.Discriminator, followItem);
+    }
+    public void UpdateFollow(Account follow)
+    {
+        uiFollows[follow.Username + "#" + follow.Discriminator].transform.GetChild(1).GetComponent<Image>().color = (follow.Status != 0) ? Color.green : Color.gray;
+    }
+
+    #region Button
+    public void OnClickAddFollow()
+    {
+        string usernameDiscriminator = addFollowInput.text;
+
+        if(!Utility.IsUsernameAndDiscriminator(usernameDiscriminator) && !Utility.IsEmail(usernameDiscriminator))
+        {
+            Debug.Log("Invalid format!");
+            return;
+        }
+
+        Client.Instance.SendAddFollow(usernameDiscriminator);
+
+    }
+
+    public void OnClickRemoveFollow(string username, string discriminator)
+    {
+        Client.Instance.SendRemoveFollow(username + "#" + discriminator);
+        uiFollows.Remove(username + "#" + discriminator);
+    }
+    #endregion
+}
