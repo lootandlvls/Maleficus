15bf677e505f494e9973712a87566c1caedb4067
leon <ga84teh@mytum.de>
2019-08-28 12:16:15 +0200

    cleaned up entry and menu scene;

--- GameProjects/Maleficus/Assets/Scripts/UI/UIContexts/LoginContext.cs
+++ GameProjects/Maleficus/Assets/Scripts/UI/UIContexts/LoginContext.cs
@@ -10,7 +10,7 @@ public class LoginContext : MonoBehaviour
     [SerializeField] protected TMP_InputField loginUsernameOrEmail;
     [SerializeField] protected TMP_InputField loginPassword;
 
-    [SerializeField] protected TextMeshProUGUI authenticationMessageText;
+    //[SerializeField] protected TextMeshProUGUI authenticationMessageText;
 
     private void Start()
     {
@@ -29,7 +29,7 @@ public class LoginContext : MonoBehaviour
 
     public void ChangeAuthenticationMessage(string msg)
     {
-        authenticationMessageText.text = msg;
+        //authenticationMessageText.text = msg;
     }
     public void EnableInputs()
     {
