e769f9016c001d0355d8fc84c57b6ac9fad410a7
leon <ga84teh@mytum.de>
2019-09-10 14:17:43 +0200

    forgot to push these

---
+++ GameProjects/Maleficus/Assets/Scripts/Events/EventHandles/WithNetMessage/GameOverEventHandle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverEventHandle : AbstractEventHandle
+{
+    public ETeamID TeamID { get; }
+
+    public GameOverEventHandle(ETeamID teamID)
+    {
+        TeamID = teamID;
+    }
+
+    public override string GetDebugMessage()
+    {
+        return "Game has ended! Winner is Team: " + TeamID;
+    }
+
+    public override AbstractNetMessage GetNetMessage()
+    {
+        return new Net_GameOver(TeamID);
+    }
+}
---
+++ GameProjects/Maleficus/Assets/Scripts/Networking/Shared/Net_Message/Net_GameOver.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class Net_GameOver : AbstractNetMessage
+{
+    public Net_GameOver()
+    {
+        ID = NetID.GameOver;
+    }
+
+    public Net_GameOver(ETeamID teamID)
+    {
+        TeamID = teamID;
+    }
+
+    public ETeamID TeamID { set; get; }
+}
