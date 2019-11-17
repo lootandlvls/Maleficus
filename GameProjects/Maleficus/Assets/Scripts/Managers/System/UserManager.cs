bb0fb48437a5d6c55cbab05f39e88e6e563785a2
sandner_leon <leonsandner@web.de>
2019-11-17 19:55:14 +0100

    fixed bugs and finished loading / creating local account data; nearly finished with local spell data;

--- GameProjects/Maleficus/Assets/Scripts/Managers/System/UserManager.cs
+++ GameProjects/Maleficus/Assets/Scripts/Managers/System/UserManager.cs
@@ -25,11 +25,8 @@ public class UserManager : AbstractSingletonManager<UserManager>
     protected override void Awake()
     {
         base.Awake();
-        /*CreateLocalData();
-        LoadSavedData();
-        UpdateSavedAccountData("", "", "", 1);
-        LoadSavedAccountData();
-        print(user.ToJson());*/
+        //CreateLocalData();
+        //LoadSavedData();
     }
 
     protected override void Start()
@@ -42,9 +39,13 @@ public class UserManager : AbstractSingletonManager<UserManager>
         base.Update();
     }
 
+    #endregion
+
+    #region Create
     public static void CreateLocalData()
     {
         CreateLocalUserAccount(false);
+        CreateLocalSpells(false);
     }
 
     public static void CreateLocalUserAccount(bool forcedCreate)
@@ -56,21 +57,63 @@ public class UserManager : AbstractSingletonManager<UserManager>
             user.account_created = new BsonDateTime(System.DateTime.Now);
             user.last_login = new BsonDateTime(System.DateTime.Now);
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Create(Application.persistentDataPath + "/savedAccountData.gd");
-            bf.Serialize(file, user);
-            file.Close();
+            FileStream file = null;
+            try
+            {
+                file = File.Create(Application.persistentDataPath + "/savedAccountData.gd");
+            }
+            catch(IOException e)
+            {
+                print(e.Message);
+                File.Delete(Application.persistentDataPath + "/savedAccountData.gd");
+                file = File.Create(Application.persistentDataPath + "/savedAccountData.gd");
+            }
+            if(file != null)
+            {
+                bf.Serialize(file, user);
+                file.Close();
+            }
         }
     }
+
+    public static void CreateLocalSpells(bool forcedCreate)
+    {
+        if(!File.Exists(Application.persistentDataPath + "/savedSpells.gd") || forcedCreate)
+        {
+            FileStream file = null;
+            try
+            {
+                file = File.Create(Application.persistentDataPath + "/savedSpells.gd");
+            }
+            catch(IOException e)
+            {
+                print(e.Message);
+                File.Delete(Application.persistentDataPath + "/savedSpells.gd");
+                file = File.Create(Application.persistentDataPath + "/savedSpells.gd");
+            }
+            if(file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+    #endregion
+
+    #region Load
 
     public static void LoadSavedData()
     {
         if(user == null)
         {
-            LoadSavedAccountData();
+            LoadSavedAccount();
+        }
+        if(spells == null)
+        {
+            LoadSavedSpells();
         }
     }
 
-    public static void LoadSavedAccountData()
+    public static void LoadSavedAccount()
     {
         if (File.Exists(Application.persistentDataPath + "/savedAccountData.gd"))
         {
@@ -83,19 +126,47 @@ public class UserManager : AbstractSingletonManager<UserManager>
             catch(SerializationException ex)
             {
                 print(ex.Message);
+                file.Close();
                 CreateLocalUserAccount(true);
+                return;
             }
 
             file.Close();
         }
     }
 
+    public static void LoadSavedSpells()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedSpells.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedSpells.gd", FileMode.Open);
+            try
+            {
+                spells = (List<Local_Spell>)bf.Deserialize(file);
+            }
+            catch (SerializationException ex)
+            {
+                print(ex.Message);
+                file.Close();
+                CreateLocalSpells(true);
+                return;
+            }
+
+            file.Close();
+        }
+    }
+
+    #endregion
+
+    #region Update
+
     public static void UpdateSavedAccountData(string user_name="", string password="", string email="", byte status=255, int coins=-1, byte level=255, int xp=-1, byte spent_spell_points=255, BsonDateTime account_created=default(BsonDateTime), BsonDateTime last_login=default(BsonDateTime))
     {
         // check if user is already loaded;
         if(user == null)
         {
-            LoadSavedAccountData();
+            LoadSavedAccount();
         }
 
         // update values if not null / default
@@ -142,8 +213,72 @@ public class UserManager : AbstractSingletonManager<UserManager>
 
         // actually update the file
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/savedAccountData.gd");
+        FileStream file = null;
+        try
+        {
+            file = File.Create(Application.persistentDataPath + "/savedAccountData.gd");
+        }
+        catch(IOException e)
+        {
+            print(e.Message);
+            bf.Serialize(file, user);
+            file.Close();
+            return;
+        }
+
         bf.Serialize(file, user);
+        file.Close();
+    }
+
+    public static void UpdateSavedSpells(bool new_spell, byte spell_id=255, bool selected = false, byte spell_level=255, int spell_xp=-1)
+    {
+        // check if spells are already loaded;
+        if (spells == null)
+        {
+            LoadSavedSpells();
+        }
+
+        // update values if not null / default
+
+        if (new_spell)
+        {
+            Local_Spell spell = new Local_Spell();
+            spell.selected = selected;
+            if (spell_id != 255)
+            {
+                spell.spell_id = spell_id;
+            }
+            if(spell_level != 255)
+            {
+                spell.spell_level = spell_level;
+            }
+            if(spell_xp != -1)
+            {
+                spell.spell_xp = spell_xp;
+            }
+            spells.Add(spell);
+        }
+        else
+        {
+            spells
+        }
+
+        // actually update the file
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = null;
+        try
+        {
+            file = File.Create(Application.persistentDataPath + "/savedSpells.gd");
+        }
+        catch (IOException e)
+        {
+            print(e.Message);
+            bf.Serialize(file, spells);
+            file.Close();
+            return;
+        }
+
+        bf.Serialize(file, spells);
         file.Close();
     }
     #endregion
