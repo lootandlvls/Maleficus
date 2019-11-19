8e229052b21fcd28eb6ddcd0423cb69b766a4f49
sandner_leon <leonsandner@web.de>
2019-11-19 19:40:21 +0100

    forgot to add the new hort for global variables;

---
+++ GameProjects/Maleficus/Assets/Scripts/Maleficus/MaleficusVariables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleficusVariables : MonoBehaviour
+{
+    /// <summary>
+    /// Contains variable members.
+    /// </summary>
+
+    #region LocalSavedData
+    // accessable variables after deserialization
+    public static Local_Account user;
+    public static List<Local_Spell> spells;
+    public static List<Local_SinglePlayer> singleplayers;
+    public static Local_Achievement achievements;
+    #endregion
+}
