9763b9b7a717e2b2992c5df6859d8411a922b5eb
sandner_leon <leonsandner@web.de>
2019-11-19 19:38:55 +0100

    forgot to add the instanceManager data class

---
+++ GameProjects/Maleficus/Assets/Scripts/Networking/Database/Model/Model_InstanceManager.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson;
+using System;
+public class Model_InstanceManager
+{
+    // identification
+    public ObjectId _id { set; get; }
+    public string ip { set; get; }
+    public string region { set; get; }
+}
