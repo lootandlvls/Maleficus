c55186ac6acc08c76b84313cfb1b3b0e0a8c380e
nassim boukadida <ga84ted@mytum.de>
2020-01-05 20:33:25 +0100

    PlayerSpellSelectionFrame scripts refactored

--- GameProjects/Maleficus/Assets/Scripts/UI/UIActions/InMenu/PlayerSpellSelectionFrame.cs
+++ GameProjects/Maleficus/Assets/Scripts/UI/UIActions/InMenu/PlayerSpellSelectionFrame.cs
@@ -15,13 +15,69 @@ public class PlayerSpellSelectionFrame : MaleficusMonoBehaviour
 
     private int spellCounter = 0;
 
+    private Dictionary<ESpellSlot, SelectedSpell> selectedSpellsIcons = new Dictionary<ESpellSlot, SelectedSpell>();
+
     // Start is called before the first frame update
     protected override void InitializeEventsCallbacks()
     {
         base.InitializeEventsCallbacks();
+
+        SpellSelectionManager.Instance.SpellButtonPressed += OnSpellButtonPressed;
         EventManager.Instance.INPUT_ButtonPressed.Event += On_INPUT_ButtonPressed_Event;
     }
-   
+
+    protected override void InitializeComponents()
+    {
+        base.InitializeComponents();
+
+        // Initialize selected spells
+        Connected.SetActive(true);
+        foreach (SelectedSpell SelectedSpell in GetComponentsInChildren<SelectedSpell>())
+        {
+            selectedSpellsIcons.Add(SelectedSpell.SpellSlot, SelectedSpell);
+            LogConsole("adding : " + SelectedSpell.SpellSlot);
+
+        }
+        LogConsole("SelectedSpell initialzed " + selectedSpellsIcons.Count);
+        Connected.SetActive(false);
+
+
+    }
+
+    private void OnSpellButtonPressed(EPlayerID playerID, AbstractSpell spell)
+    {
+        switch (spellCounter)
+            {
+            case 0:
+                if (selectedSpellsIcons[ESpellSlot.SPELL_1] != null)
+                {
+                    selectedSpellsIcons[ESpellSlot.SPELL_1].ChangeImage(spell);
+                    EventManager.Instance.Invoke_UI_SpellChosen(playerID, spell, ESpellSlot.SPELL_1);
+                }
+                break;
+            case 1:
+                if (selectedSpellsIcons[ESpellSlot.SPELL_2] != null)
+                {
+                    selectedSpellsIcons[ESpellSlot.SPELL_1].ChangeImage(spell);
+                    EventManager.Instance.Invoke_UI_SpellChosen(playerID, spell, ESpellSlot.SPELL_1);
+                }
+                break;
+            case 2:
+                if (selectedSpellsIcons[ESpellSlot.SPELL_3] != null)
+                {
+                    selectedSpellsIcons[ESpellSlot.SPELL_1].ChangeImage(spell);
+                    EventManager.Instance.Invoke_UI_SpellChosen(playerID, spell, ESpellSlot.SPELL_1);
+                }
+                break;
+
+        }
+        if (spellCounter < 3)
+        {
+            spellCounter++;
+        }
+
+    }
+
 
     private void On_INPUT_ButtonPressed_Event(NetEvent_ButtonPressed eventHandle)
     {
