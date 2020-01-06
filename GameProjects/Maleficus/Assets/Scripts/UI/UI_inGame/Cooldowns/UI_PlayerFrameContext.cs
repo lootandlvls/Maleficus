3029b5965d802851b89ce37e51d24e7590f66ed5
nassim boukadida <ga84ted@mytum.de>
2020-01-06 22:08:13 +0100

    fixed a bug

---
+++ GameProjects/Maleficus/Assets/Scripts/UI/UI_inGame/Cooldowns/UI_PlayerFrameContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_PlayerFrameContext : MaleficusMonoBehaviour
+{
+
+
+
+    private Dictionary<ESpellSlot, UI_SpellCooldowns> spellCooldownsIcons = new Dictionary<ESpellSlot, UI_SpellCooldowns>();
+
+
+    protected override void InitializeEventsCallbacks()
+    {
+        base.InitializeEventsCallbacks();
+        EventManager.Instance.SPELLS_SpellSpawned += On_SpellSpawned;
+    }
+
+    private void On_SpellSpawned(ISpell A, EPlayerID arg2)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void InitializeComponents()
+    {
+        base.InitializeComponents();
+
+        foreach (UI_SpellCooldowns SpellCooldown in GetComponentsInChildren<UI_SpellCooldowns>())
+        {
+            spellCooldownsIcons.Add(SpellCooldown.SpellSlot, SpellCooldown);
+        }
+    }
+
+
+}
---
+++ GameProjects/Maleficus/Assets/Scripts/UI/UI_inGame/Cooldowns/UI_SpellCooldowns.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_SpellCooldowns : MaleficusMonoBehaviour
+{
+
+    private Image SpellIcon;
+    public ESpellSlot SpellSlot { get { return spellSlot; } }
+    [SerializeField] EPlayerID playerID;
+    [SerializeField] ESpellSlot spellSlot;
+
+    protected override void InitializeEventsCallbacks()
+    {
+        base.InitializeEventsCallbacks();
+        EventManager.Instance.SPELLS_SpellSpawned += On_SpellSpawned;
+    }
+
+    protected override void InitializeComponents()
+    {
+        base.InitializeComponents();
+        
+        
+    }
+    // Start is called before the first frame update
+
+
+    private void On_SpellSpawned(ISpell spellCasted, EPlayerID playerID)
+    {
+       
+    }
+}
