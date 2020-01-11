95a77dd78034faa375f12f2c8a8275abee6ee385
nassim boukadida <ga84ted@mytum.de>
2020-01-11 17:40:06 +0100

    In Game Counter and arena Shrinking script

---
+++ GameProjects/Maleficus/Assets/Scripts/Environment/Arena.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public float ShrinkingRate { get { return shrinkingRate; } }
+    public float ShrinkingDuration { get { return shrinkingDuration; } }
+    public float MaxLimit { get { return maxLimit; } }
+
+    [SerializeField] float shrinkingRate;
+    [SerializeField] bool IsReadyToshrink;
+    [SerializeField] float maxLimit;
+    [SerializeField] float shrinkingDuration;
+
+    private Vector2 currentSize;
+    private Transform ArenaTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ArenaTransform = GetComponent<Transform>();
+        currentSize = new Vector2(transform.localScale.x, transform.localScale.y);
+
+
+    }
+
+
+    public void StartToShrink()
+    {
+        StartCoroutine(ShrinkCourotine());
+    }
+
+
+    private IEnumerator ShrinkCourotine()
+    {
+        float startTime = Time.time;
+        while (Time.time - startTime <= ShrinkingDuration)
+        {
+            if(currentSize.x >= 0 && currentSize.y >= 0)
+            {
+                currentSize.x -= ShrinkingRate;
+                currentSize.y -= ShrinkingRate;
+                ArenaTransform.localScale = new Vector3(currentSize.x, currentSize.y, transform.localScale.z);
+
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+      
+    }
+  
+}
