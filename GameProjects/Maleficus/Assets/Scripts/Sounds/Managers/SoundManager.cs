a7d14d2ca2f1551565ca07d485616726972a07c6
BNJMO <dj_bnjmo@hotmail.fr>
2020-01-15 00:02:03 +0100

    Removing stuff from sound manager

--- GameProjects/Maleficus/Assets/Scripts/Sounds/Managers/SoundManager.cs
+++ GameProjects/Maleficus/Assets/Scripts/Sounds/Managers/SoundManager.cs
@@ -11,71 +11,22 @@ public class SoundManager : AbstractSingletonManager<SoundManager>
     {
         base.Start();
 
-        //AppStateManager.Instance.AppStateUpdated += OnAppStateUpdated;
-        EventManager.Instance.APP_AppStateUpdated.AddListener(OnAppStateUpdated);
 
-        EventManager.Instance.SPELLS_SpellSpawned += On_SPELLS_SpellSpawned;
-        EventManager.Instance.SPELLS_SpellHitEnemy += On_SPELLS_SpellHitEnemy;
     }
 
-
-    //public SoundObject SpawnSoundObject(AudioClip audioClip, bool isLoop = false)
-    //{
-    //    GameObject gameObject = new GameObject("SO_" + audioClip.name);
-    //    //SoundObject soundObject = Instantiate<SoundObject>()
-    //}
-
-    //public SoundObject SpawnSoundObject(AudioClip audioClip, Vector3 position, bool isLoop = false)
-    //{
-
-    //}
-
-
-
-    private void On_SPELLS_SpellHitEnemy(IEnemy obj)
+    public void SpawnSoundObject(AudioClip audioClipToPlay = null, bool destroyWhenFinished = true, bool isLoop = false)
     {
-        
+        if (IS_NULL(audioClipToPlay))
+        {
+            GameObject gameObject = new GameObject("SO_" + audioClipToPlay.name);
+            //SoundObject soundObjec = Ins
+
+
+        }
     }
 
 
 
-    private void On_SPELLS_SpellSpawned(ISpell castedSpell, EPlayerID castingPlayer , ESpellSlot spellSlot)
-    {
 
-    }
-
-
-    private void OnAppStateUpdated(Event_StateUpdated<EAppState> eventHandle)
-    {
-        //switch (eventHandle.NewState)
-        //{
-        //    case EAppState.IN_MENU_IN_MAIN:
-        //        ambiamce_AudioSource.Play();
-        //        break;
-
-        //    case EAppState.IN_GAME_IN_RUNNING:
-        //        ambiamce_AudioSource.Stop();
-        //        PlayFightingMusic();
-        //        break;
-
-        //    case EAppState.IN_GAME_IN_ENDED:
-        //        fighting_AudioSource.Stop();
-        //        gameOver_AudioSource.Play();
-        //        break;
-
-        //}
-    }
-
-    //    private void PlayFightingMusic()
-    //    {
-
-    //        StartCoroutine(PlayFightingCoroutine());
-    //    }
-
-    //    private IEnumerator PlayFightingCoroutine()
-    //    {
-    //        startFighting__AudioSource.Play();
-    //        yield return new WaitForSeconds(startFighting__AudioSource.clip.length);
-    //        fighting_AudioSource.Play();
-    //    }
+    
 }
