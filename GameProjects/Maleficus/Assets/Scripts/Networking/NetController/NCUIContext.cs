06eaaa6a93a084116a6fb25f2db204985dca6199
BNJMO <dj_bnjmo@hotmail.fr>
2020-01-31 15:23:11 +0100

    NetController adjustments

---
+++ GameProjects/Maleficus/Assets/Scripts/Networking/NetController/NCUIContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace NetController
+{
+    public class NCUIContext : AbstractSingletonManager<NCUIContext>
+    {
+        [SerializeField] private ENCClientState activeOnState;
+
+        protected override void InitializeEventsCallbacks()
+        {
+            base.InitializeEventsCallbacks();
+
+            NCClient.Instance.StateUpdated += On_NCClient_StateUpdated;
+        }
+
+        private void On_NCClient_StateUpdated(ENCClientState newState)
+        {
+            if (newState == activeOnState)
+            {
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
