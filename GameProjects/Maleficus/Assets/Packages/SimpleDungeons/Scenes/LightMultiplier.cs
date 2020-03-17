86d286b73faa9bf583c67037aa46534e3275f4d7
BNJMO <dj_bnjmo@hotmail.fr>
2020-03-17 02:12:29 +0100

    Missing file from last commit

---
+++ GameProjects/Maleficus/Assets/Packages/SimpleDungeons/Scenes/LightMultiplier.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace SimpleDungeon
+{
+    public class LightMultiplier : MonoBehaviour
+    {
+        [SerializeField] private float factor = 1.0f;
+
+        public void MultiplyLight()
+        {
+            foreach (Light light in GetComponentsInChildren<Light>())
+            {
+                if (light)
+                {
+                    light.intensity *= factor;
+                }
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(LightMultiplier))]
+    public class LightMultiplierEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            LightMultiplier myTarget = (LightMultiplier)target;
+
+            if (GUILayout.Button("Multiply Light Intesity"))
+            {
+                myTarget.MultiplyLight();
+            }
+        }
+    }
+#endif
+}
